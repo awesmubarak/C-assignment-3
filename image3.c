c79f3737fb3c0f28c167ebf19cb30a0a7b8cdd5a
Awes Mubarak <contact@awesmubarak.com>
2020-05-11 18:06:27 +0100

    Add basic scaling up of images

--- image3.c
+++ image3.c
@@ -24,10 +24,10 @@ char greycode_char(int g_codes, int current_code) {
             current_run_char = '#';
             break;
         case 1:
-            current_run_char = '.';
+            current_run_char = ' ';
             break;
         case 2:
-            current_run_char = ' ';
+            current_run_char = '.';
             break;
         }
         return current_run_char;
@@ -68,10 +68,9 @@ const char *get_input(int max_len) {
 int main(int argc, char const *argv[]) {
     int width = -1, height = -1, g_levels = -1, code_or_run = 0, count = 0, number, i;
     char *token;
-    char run[] = "69 31 4 1 227 3 2 0 1 3 2 0 3 3 3 0 3 3 2 1 50 3 1 0 3 3 3 0 4 3 4 0 5 3 2 1 44 3 2 0 3 3 4 0 5 3 5 0 5 3 3 1 41 3 2 0 3 3 4 0 6 3 6 0 5 3 3 1 39 3 2 0 4 3 4 0 6 3 6 0 6 3 3 1 37 3 2 0 4 3 5 0 6 3 7 0 6 3 3 1 7 2 7 1 22 3 2 0 4 3 5 0 6 3 7 0 6 3 3 1 5 2 17 1 14 3 2 0 4 3 5 0 6 3 7 0 6 3 3 1 5 2 17 1 15 3 2 0 3 3 6 0 5 3 7 0 5 3 3 1 4 2 20 1 14 3 2 0 3 3 6 0 5 3 6 0 6 3 3 1 5 2 19 1 15 3 2 0 3 3 5 0 5 3 6 0 5 3 3 1 9 2 14 1 18 3 2 0 4 3 4 0 4 3 5 0 5 3 3 1 43 3 2 0 4 3 3 0 3 3 5 0 5 3 3 1 46 3 2 0 3 3 2 0 3 3 4 0 4 3 2 1 51 3 1 0 3 3 2 0 2 3 3 0 3 3 2 1 55 3 1 0 2 3 1 0 2 3 2 0 3 3 1 1 45 2 14 3 1 0 1 3 1 0 1 3 1 0 2 3 1 1 45 2 16 0 1 2 2 0 1 1 3 0 1 1 45 2 16 0 1 2 2 0 1 1 3 0 1 1 47 2 13 0 10 1 44 2 16 0 8 1 48 2 13 0 8 1 51 2 13 1 107 2 5 1 58 2 11 1 57 2 12 1 55 2 14 1 53 2 16";
+    char run[] = "39 26 2 1 89 0 14 1 22 0 21 1 16 0 26 1 13 0 6 1 3 0 7 1 3 0 7 1 13 0 6 1 3 0 7 1 3 0 7 1 13 0 12 1 1 0 12 1 16 0 9 1 3 0 9 1 23 0 12 1 23 0 3 1 14 0 3 1 20 0 18 1 26 0 6 1 21 0 4 1 25 0 4 1 5 0 6 1 22 0 7 1 8 0 5 1 16 0 5 1 16 0 5 1 10 0 5 1 22 0 5 1 4 0 5 1 28 0 8 1 31 0 8 1 28 0 5 1 4 0 5 1 21 0 6 1 10 0 6 1 11 0 9 1 16 0 8 1 8 0 5 1 20 0 5 1 83";
     int final_sequence[10000], current_number;
-    int gh, gv, gp, gn, max_g_l, max_g_d, max_g, p1, p2, p3, p4;
-
+    int larger_sequence[19999];
 
     token = strtok(run, " ");
     while (token != NULL) {
@@ -97,39 +96,60 @@ int main(int argc, char const *argv[]) {
         token = strtok(NULL, " ");
     }
 
+    int v_addition = 0, h_addition = 0;
+    int original_count = 0;
 
-    for (i = 0; i < (width * height); i++) {
-        if (!(i % width) || (i > (width * (height - 1)))) {
-            if (!(i % width)){
+    for (i = 0; i < ((((2 * width) - 1) * ((2 * height) - 1))); i++) {
+        if (!h_addition && !v_addition) {
+            larger_sequence[i] = final_sequence[original_count];
+
+            printf("%c", greycode_char(g_levels, final_sequence[original_count]));
+            printf("%c", final_sequence[original_count]);
+            original_count += 1;
+            if (!(original_count % 39)) {
                 printf("\n");
             }
+            // } else if (h_addition && !v_addition) {
+            /* code */
+        // } else if (!h_addition && v_addition) {
+        //     larger_sequence[i] = (final_sequence[original_count-1]+final_sequence[original_count+1])/2;
         } else {
-            p1 = final_sequence[i];
-            p2 = final_sequence[i + 1];
-            p3 = final_sequence[i + width];
-            p4 = final_sequence[i + width + 1];
+            larger_sequence[i] = 1;
+        }
 
-            gh = abs((p1 - p2 + p3 - p4) / 2);
-            gv = abs((p1 - p3 + p2 - p4) / 2);
-            gp = abs(p1 - p4);
-            gn = abs(p2 - p3);
+        // flip the horries
+        if (!(i % 78)) {
+            if (h_addition == 0) {
+                h_addition = 1;
+            } else {
+                h_addition = 0;
+            }
+        }
 
-            max_g_l = gh > gv ? gh : gv;
-            max_g_d = gp > gn ? gp : gn;
-            max_g =  max_g_l > max_g_d ? max_g_l : max_g_d;
-            printf("%c", greycode_char(g_levels, max_g));
-
+        // flip the verties
+        if (v_addition == 0) {
+            v_addition = 1;
+        } else {
+            v_addition = 0;
         }
     }
 
 
+    for (i = 0; i < (width * height); i++) {
+        printf("%c", greycode_char(g_levels, final_sequence[i]));
+        if (!(i % width)) {
+            printf("\n");
+        }
+    }
 
 
-    // for (i = 0; i < (width * height); i++) {
-    //     printf("%c", greycode_char(g_levels, final_sequence[i]));
-    //     if (!(i % width)) {
-    //         printf("\n");
-    //     }
-    // }
+    printf("\n");
+
+    for (i = 0; i < (((2 * width) - 1) * ((2 * height) - 1)); i++) {
+        printf("%c", greycode_char(2, larger_sequence[i]));
+        if (!(i % 78)) {
+            printf("\n");
+        }
+    }
     return 0;
 }
